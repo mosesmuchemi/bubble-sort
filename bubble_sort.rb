0491743e040ec4a2a0b497d5ccf8bdb27d6df957
dannkkie <ekkpodogui@gmail.com>
2019-12-20 15:37:51 +0000

    Added second function called bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,5 +15,20 @@ def bubble_sort(arr)
   arr
 end
 
+def bubble_sort_by(arr)
+  n = arr.length - 1
+  loop do
+    swapped = false
+    n.times do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  arr
+end
+
 arr = [2,3,100,3,5,4,10,7]
 puts bubble_sort(arr)
